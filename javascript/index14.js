b9474d2584bb18621a03b83da8fe9a8347216eb5
DeepanshuKant <dkdeepanshukant@gmail.com>
2021-09-04 00:07:54 +0530

    Javascript folder added

---
+++ javascript/index14.js
@@ -0,0 +1,19 @@
+console.log("hello everyone");
+console.error("this will print in error format");
+console.warn("This will print in warning format");
+
+var arr = ["hello", 5, 2, "World"];
+console.log(arr);
+console.log(arr[2]);
+
+var obj = {
+  name: "Deepanshu",
+  Age: 21,
+  arr: true,
+  "good dev": ["hello", 21, 34, "World"],
+};
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj["good dev"]);
+console.log(obj["good dev"][2]);
---
+++ javascript/index15.js
@@ -0,0 +1,5 @@
+console.log("class 15");
+function showString() {
+  console.log("thios function is called");
+}
+console.log(showString());
